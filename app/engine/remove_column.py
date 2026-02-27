872643ae7a6585fe730becac616464ff18744329
javi-diaz47 <javiereduardo300@gmail.com>
2026-02-27 14:05:10 -0500

    docs: add docstring to remove column function

--- app/engine/remove_column.py
+++ app/engine/remove_column.py
@@ -3,4 +3,14 @@ from pandas import DataFrame
 
 
 def remove_column(df: DataFrame, columns: List[str]) -> DataFrame:
+    """
+    Remove column in a dataframe
+
+    Parameters:
+        df: Input DataFrame
+        columns: List with column
+
+    Returns:
+        Dataframe without the columns specified
+    """
     return df.drop(columns=columns)
